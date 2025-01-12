057646a0ec769480e310d4594d2da16d4072b02c
NoamRon1 <Noam2009r@gmail.com>
2025-01-12 19:47:25 +0200

    code.ino done

---
+++ code/code.ino
@@ -0,0 +1,12 @@
+void setup() {
+  Serial.begin(9600);
+
+  Serial.println("Hello! this is a test!");
+  Serial.println("What is you name?");
+
+  Serial.println("My name is ____."); //HERE enter you name.
+}
+
+void loop() {
+  
+}
